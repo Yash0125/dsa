35c4c8202f5b6edd1f5a8f672a7cba7c3db976bc
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-08-15 20:49:04 +0530

    refactor : update 8.LinearSearch

--- Topic/Array/8.LinearSearch.js
+++ Topic/Array/8.LinearSearch.js
@@ -9,7 +9,7 @@ var search = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         if(nums[i]===target) return i
     }
-    
+    return -1
 };
 
 console.log(search(nums,target))
