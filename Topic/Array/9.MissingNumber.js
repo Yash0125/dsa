1184efefaee217a8e74622708f78b0f932977dfb
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-08-17 01:20:20 +0530

    feat : update 9.MissingNumber.js

--- Topic/Array/9.MissingNumber.js
+++ Topic/Array/9.MissingNumber.js
@@ -6,10 +6,9 @@ function missingNumber(nums) {
     // Calculate the expected sum and the actual sum of the numbers
     for (let i = 0; i <= n; i++) {
         expectedSum += i;
-        actualSum += nums[i] || 0; // Handle the case when nums[i] is undefined (out of bounds)
+        actualSum += nums[i] || 0; 
     }
 
-    // The missing number is the difference between the expected sum and the actual sum
     return expectedSum - actualSum;
 }
 
@@ -20,4 +19,21 @@ console.log(missingNumber(nums1)); // Output: 2
 const nums2 = [0, 1];
 console.log(missingNumber(nums2)); // Output: 2
 
+
+// you can also do it with one loop of reduce for nums(given array) and other for loop for 
+
+var missingNumber = function(nums) {
+    let n = nums.length + 1;
+    let expectedTotal = 0;
+  
+    let realTotal = nums.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+  
+    for (let i = 0; i < n; i++) {
+      expectedTotal += i;
+    }
+    return expectedTotal - realTotal;
+      
+  };
 
