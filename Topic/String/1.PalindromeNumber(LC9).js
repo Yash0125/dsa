abbd74d8a9cc52e8ba38a2a51a786433017da6b5
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-10-13 18:42:24 +0530

    feat : 1.PalindromeNumber(LC9) created

---
+++ Topic/String/1.PalindromeNumber(LC9).js
@@ -0,0 +1,19 @@
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+let x=121
+
+var isPalindrome = function(x) {
+    if(x.length<2) return false
+    return x===+x.toString().split('').reverse().join('')
+};
+
+console.log(isPalindrome(x))
