70585d0b2a9b65af4b024c14919fd1c51cf4c333
Yash <116873935+Yash0125@users.noreply.github.com>
2023-10-23 13:30:58 +0530

    fix : Update 3.leftRotateBy1.js

--- Topic/Array/3.leftRotateBy1.js
+++ Topic/Array/3.leftRotateBy1.js
@@ -8,7 +8,7 @@ let arr = [1, 2, 3, 4, 5];
 var leftRotate = function (arr) {
   let temp = arr[0];
   let n = arr.length;
-  for (let i = 0; i < n; i++) {
+  for (let i = 1; i < n; i++) {
     arr[i - 1] = arr[i];
   }
   arr[n - 1] = temp;
