31bd5892647a51ac0b229707afc409bdbcdd2fa6
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-10-23 17:58:03 +0530

    feat : 5.ValidAnagram(LC242).js added

---
+++ Topic/String/5.ValidAnagram(LC242).js
@@ -0,0 +1,48 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+// method 1
+
+let s = "anagram";
+let t = "nagaram";
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let obj1 = {};
+  let obj2 = {};
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isAnagram(s, t));
+
+// method 2
+
+const isAnagram1 = function (s, t) {
+  s = s.split("").sort().join("");
+  t = t.split("").sort().join("");
+
+  return s === t;
+};
+
+console.log(isAnagram1(s, t));
+
