0043aa950c7c58793ebba0d929cbe7dfc50892d4
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-03-31 13:37:57 +0530

    refactor : 5.RotateArrayByk.js updated

--- Topic/Array/5.RotateArrayByk.js
+++ Topic/Array/5.RotateArrayByk.js
@@ -5,25 +5,48 @@
 // rotate 2 steps to the right: [6,7,1,2,3,4,5]
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-
 // brute force approach
 
-let nums = [1,2,3,4,5,6,7];
-let k=3;
+let nums = [1, 2, 3, 4, 5, 6, 7];
+let k = 3;
 
-
-var rotate = function(nums, k) {
-  let size=nums.length;
-  if(k>size){
-    k=k%size;
+var rotate = function (nums, k) {
+  let size = nums.length;
+  if (k > size) {
+    k = k % size;
   }
-  const rotated=nums.splice(size-k,size)
-  nums.unshift(...rotated)
+  const rotated = nums.splice(size - k, size);
+  nums.unshift(...rotated);
   return nums;
-    
 };
 
-console.log(rotate(nums, k))
+console.log(rotate(nums, k));
+
+function rotateArrayBetter(nums, k) {
+  let n = nums.length;
+  k = k % n;
+  let temp = [];
+
+  for (let i = 0; i < k; i++) {
+    temp[i] = nums[i];
+    // from above loop we'll get[1,2,3]
+  }
+
+  for (let i = 0; i < n - k; i++) {
+    nums[i] = nums[k + i];
+  }
+  // from above loop we'll get [4, 5, 6, 7,5, 6, 7]
+
+  for (let i = n - k; i < n; i++) {
+    nums[i] = temp[i - k - 1];
+  }
+  return nums;
+}
+
+console.log(rotateArrayBetter(nums, k));
+
+// time O(n+d)
+// space O(d)
 
 function rotateArrayOptimised(nums, k) {
   let size = nums.length;
@@ -32,13 +55,12 @@ function rotateArrayOptimised(nums, k) {
     k = k % size;
   }
 
-  reverse(nums, 0, nums.length - 1); // O(n)
   reverse(nums, 0, k - 1); // O(k)
   reverse(nums, k, nums.length - 1); // O(n-k)
+  reverse(nums, 0, nums.length - 1); // O(n)
 
   return nums;
 }
-
 
 function reverse(nums, left, right) {
   while (left < right) {
@@ -48,5 +70,7 @@ function reverse(nums, left, right) {
   }
 }
 
+console.log(rotateArrayOptimised(nums, k));
 
-console.log(rotateArrayOptimised(nums, k))
+// time O(n)
+// space O(1)
