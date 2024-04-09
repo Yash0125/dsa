ed1574e6b5c6eed5860443c2893f37d18acc003a
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-04-09 19:51:45 +0530

    feat : 1.ReverseWordsInString.js created

--- Topic/Stack/1.ReverseWordsInString.js
+++ Topic/Stack/1.ReverseWordsInString.js
@@ -1,8 +1,8 @@
 // Ques 1 : Given an input string s, reverse the order of the words
 
-// Input: "the sky is blue"  
+// Input: "the sky is blue"
 // Output: "blue is sky the"
-// Input: "  hello world  " 
+// Input: "  hello world  "
 // Output: "world hello"
 // Input: "a good   example"
 // Output: "example good a"
@@ -10,14 +10,29 @@
 // "the sky is blue" => [the,sky,is,blue]
 // [] => [the,sky,is,blue] => blue is sky the
 
+let s = "the sky is blue";
 
-let s= "the sky is blue";
+const reverseWords = (s) => {
+  let splitS = s.split(" ");
+  const stack = [];
 
-const reverseWords=(s)=>{
-    let split = s.split(" ")
-}
+  for (let i of splitS) {
+    stack.push(i);
+  }
 
-console.log(reverseWords(s))
+  let finals = "";
 
+  while (stack.length) {
+    const current = stack.pop();
 
-// 1.ReverseWordsInString.js
+    if (current) {
+      finals += " " + current;
+    }
+  }
+  return finals.trim();
+};
+
+console.log(reverseWords(s));
+
+// TC : O(2n) = O(n)
+// SC : O(2n) = O(n)
