fa10c09aa7bdf1b8cf3351c572863861f12fb4b1
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-04-13 18:51:45 +0530

    feat : 23.IntersectionTwoArraysII(LC350).js created

---
+++ Topic/Array/23.IntersectionTwoArraysII(LC350).js
@@ -0,0 +1,72 @@
+// 350. Intersection of Two Arrays II
+// Easy
+// Topics
+// Companies
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+
+let nums1 = [1, 2, 2, 1];
+let nums2 = [2, 2];
+
+// brute force
+
+var intersect = function (nums1, nums2) {
+  let n1 = nums1.length;
+  let n2 = nums2.length;
+  let ans = [];
+  let visited = new Array(nums2.length).fill(0);
+  for (let i = 0; i < n1; i++) {
+    for (let j = 0; j < n2; j++) {
+      if (nums1[i] === nums2[j] && visited[j] === 0) {
+        ans.push(nums2[j]);
+        visited[j] = 1;
+        break;
+      } else if (nums2[j] > nums1[i]) {
+        break;
+      }
+    }
+  }
+  return ans;
+};
+
+console.log(intersect(nums1, nums2));
+
+// TC : O(n1*n2)
+// SC : O(n2)
+
+// optimized using 2 pointer
+
+var intersectOptimized = function (nums1, nums2) {
+  let n1 = nums1.length;
+  let n2 = nums2.length;
+  let i = 0;
+  let j = 0;
+  let ans = [];
+
+  while (i < n1 && j < n2) {
+    if (nums1[i] < nums2[j]) {
+      i++;
+    } else if (nums1[i] > nums2[j]) {
+      j++;
+    } else {
+      ans.push(nums1[i]);
+      i++;
+      j++;
+    }
+  }
+  return ans;
+};
+
+console.log(intersectOptimized(nums1, nums2));
+
+// TC : O(n1 + n2)
+// SC : O(1)
