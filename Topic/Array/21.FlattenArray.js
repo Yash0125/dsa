43cc60d2b4bbef70a8537cd7351a9eb45de96fa8
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-04-21 23:03:56 +0530

    refactor : FlattenArray.js updated

--- Topic/Array/21.FlattenArray.js
+++ Topic/Array/21.FlattenArray.js
@@ -13,3 +13,50 @@ function flattenArray(nums) {
 }
 
 console.log(flattenArray(nums));
+
+// other type  of quetion but method is same
+
+// Write a function to return the count of numbers in an Array | MakeMyTrip Frontend Interview Question
+// In this question, the candidate needs to implement a function that returns the count of
+//  numbers in a provided array.
+
+// The array might contain other data types and nested arrays too.
+
+// Many users have reported that this question was asked in the frontend interview process of
+// companies like MakeMyTrip.
+
+// Syntax
+// countNumbers(collection);
+// Arguments
+// collection (Array): The input array.
+// Return
+// the total count of numbers inside the array.
+// Example
+
+// countNumbers([ 1,"2", [3,4,[5]], function(){}, 8, 9 ]);
+// // 6
+
+// countNumbers([]);
+// // 0
+
+let collection = [1, "2", [3, 4, [5]], function () {}, 8, 9];
+
+function countNumbers(collection) {
+  // write your solution below
+
+  let count = 0;
+  if (!collection.length) {
+    return count;
+  }
+  for (let i = 0; i < collection.length; i++) {
+    if (typeof collection[i] === "number") {
+      count += 1;
+    } else if (Array.isArray(collection[i])) {
+      count += countNumbers(collection[i]);
+    }
+  }
+
+  return count;
+}
+
+console.log(countNumbers(collection));
